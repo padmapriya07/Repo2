6e6c84d14806be6549987e39db636c8efd206b32
padma <yaswin302015@gmail.com>
2020-11-04 15:08:43 -0500

    first commit

---
+++ return/src/mysecond/mysecondpro.java
@@ -0,0 +1,16 @@
+
+package mysecond;
+
+public class mysecondpro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+        int a=2, b=4,c=5, sum;
+        
+              sum=a+b;
+              System.out.println("sum="+sum);
+              
+	}
+
+}
