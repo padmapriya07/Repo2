4b0e0feaf7b34620611ce27f0f9d0572d5b071ee
padma <yaswin302015@gmail.com>
2020-11-04 15:16:05 -0500

    added new numbers

--- return/src/mysecond/mysecondpro.java
+++ return/src/mysecond/mysecondpro.java
@@ -6,9 +6,10 @@ public class mysecondpro {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         
-        int a=2, b=4,c=5, sum;
+        int a=12, b=4,c=50, sum;
         
               sum=a+b;
+              sum=a+c;
               System.out.println("sum="+sum);
               
 	}
